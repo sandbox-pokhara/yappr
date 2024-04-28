ea1224b85a755ad1bbe55c4295556c3a1f645b88
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2024-04-28 10:12:46 +0545

    refactor: change yappr decorator params

--- yappr/decorator.py
+++ yappr/decorator.py
@@ -1,26 +1,22 @@
-# WARNING: ONLY IMPORT STANDARD LIBRARY
-# HERE, THIS REDUCES THE CHANCES OF SCRIPT CRASHING
 import os
 import sys
 import tkinter as tk
 import traceback
 from argparse import ArgumentParser
-from threading import Event
 from threading import Thread
 from tkinter.messagebox import showerror  # type: ignore
 from typing import Any
 from typing import Callable
 from typing import ParamSpec
 
+from yappr import logger
+from yappr.updater import Updater
+
 Param = ParamSpec("Param")
 
 
 def yappr(
-    pkg_name: str,
-    exit_flag: Event,
-    new_update_flag: Event,
-    first_update_interval: float = 10,
-    update_interval: float = 900,
+    updater: Updater,
 ) -> Callable[[Callable[Param, Any]], Callable[Param, None]]:
 
     def decorator(
@@ -28,21 +24,8 @@ def yappr(
     ) -> Callable[Param, None]:
 
         def wrapped(*args: Param.args, **kwargs: Param.kwargs) -> None:
-            # WARNING: ONLY USE STANDARD LIBRARY
-            # AND UPDATER MODULE HERE, THIS REDUCES THE
-            # CHANCES OF SCRIPT CRASHING
-            from yappr import logger
-            from yappr.updater import Updater
-
             # Initialize updater, updater needs to start as
             # soon as the script starts
-            updater = Updater(
-                pkg_name,
-                first_update_interval,
-                update_interval,
-                exit_flag,
-                new_update_flag,
-            )
             parser = ArgumentParser()
             parser.add_argument("-nu", "--no-updater", action="store_true")
             cmd_args, _ = parser.parse_known_args()
@@ -56,11 +39,11 @@ def yappr(
                     out = func(*args, **kwargs)
 
                     # Wait until exit flag is set
-                    exit_flag.wait()
+                    updater.exit_flag.wait()
                     logger.info("Gracefully shutting down...")
 
                     # If exit flag was set by user, stop the script
-                    if not new_update_flag.is_set():
+                    if not updater.new_update_flag.is_set():
                         return
 
                     # if exit flag was set by updater
@@ -76,7 +59,7 @@ def yappr(
                         parent=root,
                     )
                     if out == "ok":
-                        exit_flag.set()
+                        updater.exit_flag.set()
                         return
 
         return wrapped
